2d11aff07e9d6a48eb03b1d56f25631d8a364873
muhumbania <franklinmuhumbania@gmail.com>
2024-03-17 22:41:40 +0300

    Added the route for the home page

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render("index.ejs");
+});
 
 app.listen(port, ()=>{
     console.log(`Server listening on port ${port}`);
